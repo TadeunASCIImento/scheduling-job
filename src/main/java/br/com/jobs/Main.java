9d4b355a67fd1ba43ecfdb36f1d1bc9d7d45ae39
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-09-06 22:51:42 -0300

    [add] Classe Main.java

--- src/main/java/br/com/jobs/Main.java
+++ src/main/java/br/com/jobs/Main.java
@@ -1,9 +1,19 @@
 package br.com.jobs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import br.com.jobs.models.Job;
+import br.com.jobs.models.Scheduling;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scheduling scheduling = new Scheduling();
+		List<ArrayList<Job>> escalonados = scheduling.escalonarJobsParaExecucao(new Scheduling().generateListJobs());
+		System.out.println(new Gson().toJson(escalonados));
 	}
 
 }
